b64d50d27ef979687654cd8ccc7d9c6e50ce37bf
Leticia-Sousa <leticiasousa917@gmail.com>
2021-07-04 19:51:51 -0300

    atualização

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -4,7 +4,19 @@ package entidades;
 
 public class Cliente {
     
+    public int codigo;
     public String nome;
     public String endereco;
+    public int numero;
+    public String complemento;
+    public String cep;
+    public String bairro;
+    public String cidade;
+    public String uf;
+    public String email;
+    public String celular;
+    public String fixo;
+    public String rg;
+    public String cpf;
     
 }
--- src/principal/SistemaDeCadastroDeClientes.java
+++ src/principal/SistemaDeCadastroDeClientes.java
@@ -17,11 +17,27 @@ public class SistemaDeCadastroDeClientes {
         Cliente c1 = new Cliente();
         
         System.out.println("Digite os dados do cliente");
+        System.out.println("Digite o codigo: ");
+        c1.codigo = sc.nextInt();
         System.out.println("Digite o nome: ");
+        sc.nextLine();
         c1.nome = sc.nextLine();
         System.out.println("Digite o endereco: ");
         c1.endereco = sc.nextLine();
-        
+        System.out.println("Digite o numero: ");
+        c1.numero = sc.nextInt();
+        System.out.println("Digite o complemento: ");
+        sc.nextLine();
+        c1.complemento = sc.nextLine();
+        System.out.println("Digite o celular: ");
+        c1.celular = sc.next();
+        System.out.println("Digite o fixo: ");
+        c1.fixo = sc.next();
+        System.out.println("Digite o rg: ");
+        sc.nextLine();
+        c1.rg = sc.nextLine();
+        System.out.println("Digite o cpf: ");
+        c1.cpf = sc.nextLine();
     }
     
 }
