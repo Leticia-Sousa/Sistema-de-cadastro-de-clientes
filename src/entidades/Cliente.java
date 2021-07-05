093d176f83e7e5b70a62314de8f57808f2c0b8c7
Leticia-Sousa <leticiasousa917@gmail.com>
2021-07-05 00:03:44 -0300

    atualização

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -7,6 +7,7 @@ public class Cliente {
     public int codigo;
     public String nome;
     public String endereco;
+    public String nascimento;
     public int numero;
     public String complemento;
     public String cep;
@@ -18,5 +19,12 @@ public class Cliente {
     public String fixo;
     public String rg;
     public String cpf;
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "Codigo =" + codigo + ", Nome =" + nome + ", Endereco =" + endereco + ", Nascimento =" + nascimento + ", Numero =" + numero + ", Complemento =" + complemento + ", Cep =" + cep + ", Bairro =" + bairro + ", Cidade =" + cidade + ", Uf =" + uf + ", Email =" + email + ", Celular =" + celular + ", Fixo =" + fixo + ", Rg =" + rg + ", Cpf =" + cpf + '}';
+    }
+    
+    
     
 }
--- src/principal/SistemaDeCadastroDeClientes.java
+++ src/principal/SistemaDeCadastroDeClientes.java
@@ -16,28 +16,44 @@ public class SistemaDeCadastroDeClientes {
         
         Cliente c1 = new Cliente();
         
-        System.out.println("Digite os dados do cliente");
+        System.out.println("DIGITE OS DADOS DO CLIENTE");
         System.out.println("Digite o codigo: ");
         c1.codigo = sc.nextInt();
         System.out.println("Digite o nome: ");
         sc.nextLine();
         c1.nome = sc.nextLine();
+        System.out.println("Digite o data de nascimento: ");
+        c1.nascimento = sc.nextLine();
         System.out.println("Digite o endereco: ");
         c1.endereco = sc.nextLine();
-        System.out.println("Digite o numero: ");
+        System.out.println("Digite o numero da residência: ");
         c1.numero = sc.nextInt();
         System.out.println("Digite o complemento: ");
         sc.nextLine();
         c1.complemento = sc.nextLine();
+        System.out.println("Digite o cep: ");
+        c1.cep = sc.nextLine();
+        System.out.println("Digite o bairro: ");
+        c1.bairro = sc.nextLine();
+        System.out.println("Digite a cidade: ");
+        c1.cidade = sc.nextLine();
+        System.out.println("Digite o uf: ");
+        c1.uf = sc.next();
+        System.out.println("Digite o email: ");
+        sc.nextLine();
+        c1.email = sc.nextLine();
         System.out.println("Digite o celular: ");
         c1.celular = sc.next();
         System.out.println("Digite o fixo: ");
+        sc.nextLine();
         c1.fixo = sc.next();
         System.out.println("Digite o rg: ");
         sc.nextLine();
         c1.rg = sc.nextLine();
         System.out.println("Digite o cpf: ");
         c1.cpf = sc.nextLine();
+        
+        System.out.println(c1.toString());
     }
     
 }
